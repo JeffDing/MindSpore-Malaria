210c235fe7974a95ff27cc13d9154652df48f783
JeffDing <jeffding@noreply.localhost>
2024-02-21 14:12:59 +0800

    set save_checkpoint_steps

--- train_openi.py
+++ train_openi.py
@@ -31,6 +31,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--device_target', type=str, default="Ascend", choices=['Ascend', 'GPU', 'CPU'],help='device where the code will be implemented (default: Ascend)')
 parser.add_argument('--data_path', default="/cache/data", type=str, help='data path')
 parser.add_argument("--epochs", default=10, type=int, help="epochs")
+parser.add_argument("--save_checkpoint_steps", default=10, type=int, help="epochs")
 
 parser.add_argument('--data_url', metavar='DIR', default='', help='path to dataset')
 parser.add_argument('--train_url', metavar='DIR', default='', help='save output')
@@ -103,7 +104,7 @@ network_loss = CrossEntropySmooth(sparse=True,
                                   smooth_factor=0.1,
                                   num_classes=num_classes)
 # set checkpoint
-ckpt_config = CheckpointConfig(save_checkpoint_steps=step_size, keep_checkpoint_max=100)
+ckpt_config = CheckpointConfig(save_checkpoint_steps=args.save_checkpoint_steps, keep_checkpoint_max=100)
 ckpt_callback = ModelCheckpoint(prefix='vit_b_16', directory=train_dir, config=ckpt_config)
 
 # initialize model
